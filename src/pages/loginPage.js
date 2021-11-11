466e3b0e0fa3c2206676effb9903ea6b792b6193
Yoseph <yosephtaddese111@gmail.com>
2021-11-11 16:54:52 +0300

    image size fix for and loading indicator for login

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,16 +1,25 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import FirebaseApp from '../Firebase'
 import flower from '../assets/img/flower.png'
 import flame1 from '../assets/img/flame-1.png'
 import banner from '../assets/img/banner-full.png'
 import { useNavigate } from 'react-router-dom'
+import Loader from "react-loader-spinner";
+import Popup from 'reactjs-popup';
+import { ToastContainer, toast, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'reactjs-popup/dist/index.css';
 
 const auth = getAuth(FirebaseApp);
 
 function LoginPage() {
     const [name, setName] = useState('')
     const [phone, setPhone] = useState('')
+    const [errors, setErrors] = useState({})
+    const [open, setOpen] = useState(false);
+    const closeModal = () => setOpen(false);
+
     let navigate = useNavigate()
 
     useLayoutEffect(() => {
@@ -18,8 +27,7 @@ function LoginPage() {
         page.style.minHeight = window.innerHeight + 'px'
     }, [])
 
-
-    useEffect(() => {
+    useLayoutEffect(() => {
         window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
             'size': 'invisible',
             'callback': (response) => {
@@ -44,15 +52,40 @@ function LoginPage() {
                     <label htmlFor="" className="form__label">NAME:</label>
                     <input onChange={e => setName(e.target.value)} type="text" value={name} className="form__input" />
                 </div>
+                <span className="form__error">{errors["name"]}</span>
                 <div className="form__group">
                     <label htmlFor="" className="form__label">MOBILE NO:</label>
                     <input onChange={e => setPhone(e.target.value)} type="tel" value={phone} className="form__input" />
                 </div>
+                <span className="form__error">{errors["phone"]}</span>
                 {/* {!loading && ()} */}
-                <button id="sign-in-button" type="submit" className="img-btn form__btn">
-                    Send
-                </button>
+                    <button id="sign-in-button" type="submit" className="img-btn form__btn">
+                        Send
+                    </button>
+                {/* {!loading && (
+                )} */}
+                {/* {loading && (
+                    <Loader
+                        type="ThreeDots"
+                        color="#FEFEFE"
+                        height={50}
+                        width={50} //3 secs
+                    />
+                )} */}
             </form>
+
+            <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+                <div className="modal">
+                    <Loader
+                        type="TailSpin"
+                        color="#FEFEFE"
+                        height={40}
+                        width={40} //3 secs
+                    />
+                    <span className="modal__text">Logging in</span>
+                </div>
+            </Popup>
+            <ToastContainer theme="dark" transition={Slide} />
 
             <img src={flower} alt="" className="floating-img floating-img--1" />
             <img src={flower} alt="" className="floating-img floating-img--2" />
@@ -63,14 +96,34 @@ function LoginPage() {
         </div>
     )
 
+    function handleValidation() {
+        let fields = { name, phone };
+        let errs = {};
+        let formIsValid = true;
+
+        // Name
+        if (!fields["name"]) {
+            formIsValid = false;
+            errs["name"] = "Name cannot be empty";
+        }
+        // Phone
+        if (!fields["phone"]) {
+            formIsValid = false;
+            errs["phone"] = "Phone cannot be empty";
+        }
+
+        setErrors(errs)
+        return formIsValid;
+    }
+
     function onSubmitHandler(e) {
         e.preventDefault()
         // navigate("/players")
 
-        if (!name || !phone) {
+        if (!handleValidation()) {
             return
         }
-        // setLoading(true)
+        setOpen(true)
         console.log("submitting")
         signInWithPhoneNumber(auth, phone, window.recaptchaVerifier)
             .then((confirmationResult) => {
@@ -81,9 +134,23 @@ function LoginPage() {
                 navigate("/players")
                 // ...
             }).catch((error) => {
-                console.log(error)
-                navigate("/players")
                 // Error; SMS not sent
+                error = new Error(error).toString().toLowerCase()
+                console.log(error)
+                let errorMsg = "Couldn't log you in"
+                if(error.includes('invalid format')) {
+                    errorMsg = "Invalid phone number. Check your input."
+                }
+                closeModal()
+                toast(errorMsg, {
+                    position: "bottom-center",
+                    autoClose: 4500,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    progress: undefined,
+                    });
 
                 // ...
             });
