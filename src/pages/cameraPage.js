fdebc3f98a3ae6b8380cb7dcfebcf9a61bd085cb
Yoseph <yosephtaddese111@gmail.com>
2021-11-18 12:25:34 +0300

    phone input alignment change & camera aspect ratio

--- src/pages/cameraPage.js
+++ src/pages/cameraPage.js
@@ -1,17 +1,16 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { } from 'react'
 import QuestionOverlay from '../components/questionOverlay'
 import Webcam from 'react-webcam'
 
 function CameraPage() {
-    const [width, setWidth] = useState("100%")
-    const [height, setHeight] = useState("100%")
-    useLayoutEffect(() => {
-        let w = window.innerWidth + 'px'
-        let h = window.innerHeight + 'px'
-        setWidth(w)
-        setHeight(h)
-        console.log(w, h)
-    }, [])
+    // const [width, setWidth] = useState("100%")
+    // const [height, setHeight] = useState("100%")
+    // useLayoutEffect(() => {
+    //     let w = (window.innerWidth < 768 ? window.innerWidth : 768) + 'px'
+    //     let h = window.innerHeight + 'px'
+    //     setWidth(w)
+    //     setHeight(h)
+    // }, [])
 
     const videoContraints = {
         facingMode: "user",
@@ -19,7 +18,7 @@ function CameraPage() {
     }
     return (
         <div className="camera">
-            <Webcam width={width} height={height}
+            <Webcam className="video-tag"
                 videoConstraints={videoContraints} mirrored={true}
                 audio={false} onUserMediaError={onMediaError} />
             <QuestionOverlay />
