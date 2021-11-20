d79125cbcea9191f6ba58ff00317cb47b5918dca
Yoseph <yosephtaddese111@gmail.com>
2021-11-20 18:01:59 +0300

    set up music request modal

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { LoginPage, WelcomePage } from './pages';
 import { Routes, Route } from 'react-router-dom';
 import UserContext from './_helpers/userContext';
 import RouteContext from './_helpers/routeContext';
-import soundfile from './assets/audio/chinese_new_year.mp3'
-import Sound from 'react-sound'
-import { useState } from 'react';
+import soundfile from './assets/audio/chinese_new_year.ogg'
+// import Sound from 'react-sound'
+import Popup from 'reactjs-popup';
+import { useLayoutEffect, useState } from 'react';
 import { ProtectedLinksPage, ProtectedPlayersPage } from './components';
 import ProtectedGamePlayPage from './components/ProtectedGamePlayPage';
 
@@ -14,19 +15,27 @@ function App() {
   const userStr = localStorage.getItem('_user')
   let userData;
   try {
-      userData = JSON.parse(userStr)
+    userData = JSON.parse(userStr)
   } catch (e) {
-      userData = null
+    userData = null
   }
   let pathData;
   try {
-      pathData = JSON.parse(pathStr)
+    pathData = JSON.parse(pathStr)
   } catch (e) {
-      pathData ={}
+    pathData = {}
   }
   const [user, setUser] = useState(userData)
   const [path, setPath] = useState(pathData)
-  
+  const [open, setOpen] = useState(false);
+  const toggleModal = (state) => setOpen(state);
+
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      toggleModal(true)
+    }, 4500);
+  }, [])
+
   return (
     <UserContext.Provider value={{ user, storeUser }}>
       <RouteContext.Provider value={{ path, storePath }}>
@@ -48,12 +57,29 @@ function App() {
           <Route path="my/game" element={<ProtectedGamePlayPage />} />
         </Routes>
       </RouteContext.Provider>
-      <Sound
+      {/* <Sound
         url={soundfile}
         playStatus={Sound.status.PLAYING}
         autoLoad={true}
         loop={true}
-      />
+      /> */}
+      <Popup contentStyle={{width: '70%'}} open={open} closeOnDocumentClick={false} onClose={() => toggleModal(false)} position="bottom center">
+        <div className="modal modal--audio">
+          <div className="content">
+            {' '}
+            How about a music as you play?
+          </div>
+          <div className="actions">
+            <button className="button" onClick={() => toggleModal(false)}>
+              No
+            </button>
+            <button className="button" onClick={playSound}>
+              Yes
+            </button>
+          </div>
+        </div>
+      </Popup>
+      <audio id="audio" src={soundfile}></audio>
     </UserContext.Provider>
   );
 
@@ -67,6 +93,13 @@ function App() {
     setPath(path)
     const pathStr = JSON.stringify(path)
     localStorage.setItem("_path", pathStr)
+  }
+
+  function playSound() {
+    toggleModal(false)
+    const audio = document.getElementById('audio')
+    audio.volume = 0.5
+    audio.play()
   }
 }
 
